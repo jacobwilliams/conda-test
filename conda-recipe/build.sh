98869c498149c6fc16c9426dc2b9cbde63cfba51
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-02-18 22:18:32 -0600

    update build.sh path

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,4 +3,8 @@
 # install using pip from the whl file 
 
 #.... can we make this a local path???
-pip install ~/git/conda-test/src/dist/foojr-0.0.1-py3-none-any.whl --no-deps
+# pip install ~/git/conda-test/src/dist/foojr-0.0.1-py3-none-any.whl --no-deps
+
+#... this does work, seems like we should have an env var for src somewhere?
+# (does it work on windows?)
+pip install ${RECIPE_DIR}/../src/dist/foojr-0.0.1-py3-none-any.whl --no-deps
