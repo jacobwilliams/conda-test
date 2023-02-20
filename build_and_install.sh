636ec4ddb9fa6a925664143fdca35b69bf48eccb
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-02-19 21:27:14 -0600

    note

--- build_and_install.sh
+++ build_and_install.sh
@@ -17,6 +17,10 @@ gfortran --shared ./src/foojr/foo.f90 -o ./src/foojr/foo.dylib
 
 python -m build ./src --outdir ./build-output 
 
+# specify platform ??
+# doesn't work... get "is not a supported wheel on this platform" error ... 
+# python -m build ./src --outdir ./build-output -w -C="--global-option=--plat-name" -C="--global-option=osx-arm64"
+
 conda build ./conda-recipe --output-folder conda-output
 conda build purge
 conda install ./conda-output/osx-arm64/foojr-0.0.1-py39_1.tar.bz2
